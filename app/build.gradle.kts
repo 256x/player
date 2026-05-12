e4f1f2c37292b5196a77ef3664c3a406e6a7f822
256x <admin@fumi.day>
2026-05-12 13:00:53 +0900

    Bump version to 1.3.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,8 +41,8 @@ android {
         applicationId = "fumi.day.literalplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2.1"
+        versionCode = 7
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
